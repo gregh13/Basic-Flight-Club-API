7d1554d6880bc111ae8b7c34c115521ecd7ac004
gregh13 <99034583+gregh13@users.noreply.github.com>
2022-04-11 10:12:20 +0700

    Added the Surprise Me feature to main

--- main.py
+++ main.py
@@ -42,29 +42,37 @@ if day_of_week == 5:
         price_ceiling_dict = {destination_list[x-1]: user[f"price{x}"] for x in range(1, 6)}
         print(price_ceiling_dict)
         for destination in destination_list:
+            price_ceiling = price_ceiling_dict[destination]
             print(destination)
-            tequila = FlightSearcher(flying_from, destination, API)
+            if "Surprise" in destination:
+                destination = random.choice(capitol_cities)
+            print(destination)
+            tequila = FlightSearcher(flying_from, destination)
             city_code = tequila.get_iata_code()
             print(city_code)
+            if city_code is None:
+                print("None caught")
+                continue
             flight_data = tequila.look_for_flights()
             if "error" in flight_data:
+                print("EERRROOOORRRR!!!!")
                 notify_dev = True
                 continue
             elif len(flight_data["data"]) == 0:
+                print(f"{city_code} has no results")
                 continue
             else:
                 flight_info = FlightInfo(flight_data, destination)
-                user_price_ceiling = price_ceiling_dict[destination]
-                if flight_info.price <= user_price_ceiling:
+                if flight_info.price <= price_ceiling:
                     print(f"The price is right for {flying_from}-{city_code}")
                     email_message = flight_info.deal_info_to_message()
                     flight_deal_message_list += email_message
         if len(flight_deal_message_list) != 0:
-            print(f"Number of deals: {round(len(flight_deal_message_list)/422)}")
-            email_sender = EmailSender(user_name, user_email, flight_deal_message_list, EMAIL, PASS)
+            print(f"Number of deals: {round(len(flight_deal_message_list)/500)}")
+            email_sender = EmailSender(user_name, user_email, flight_deal_message_list)
             email_sender.send_email()
         if notify_dev:
-            sender = NotifySMS(SID, TOKEN, NUM_FROM, NUM_TO)
+            sender = NotifySMS()
             sender.send_sms(user_name)
 else:
     print("It's not time yet!")
